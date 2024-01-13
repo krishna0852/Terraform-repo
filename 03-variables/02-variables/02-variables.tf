650a3a33c3211acb6bcd3daf981ee3e5835c8425
Krishna Teja Palla <krishnatejapalla1@gmail.com>
2024-01-13 17:34:19 +0530

    defining map and list variables in terraform

---
+++ 03-variables/02-variables/02-variables.tf
@@ -0,0 +1,45 @@
+provider "aws"{
+    access_key=var.credentials["akey"]
+    secret_key=var.credentials["skey"]
+    region="ap-southeast-2"
+}
+
+resource "aws_instance" "ec2-launch"{
+    ami=var.ami
+    instance_type=var.instance_types[0]
+    key_name = var.key
+
+    tags={
+        Name="test"
+        executebyterraform=true
+    }
+}
+
+variable "credentials" {
+type=map(string)
+default={
+    "akey"="AKIA2UC3CSCWRMXCKD6L"
+    "skey"="k/xZusH4gE6UlnrebD8MqXdwnwh0yRwvPcUi771R"
+}  
+}
+
+variable "ami" {
+    type=string
+    default = "ami-04f5097681773b989"
+
+}
+
+variable "instance_types"{
+    type=list(string)
+    default = [ "t3.micro","t3.medium","t3.large" ]
+}
+
+variable "key" {
+    type=string
+    default = "bashscript"
+
+}
+
+
+
+  
