9f82306f49eea674ff4bdb92f1785c03ca9b447a
Krishna Teja Palla <krishnatejapalla1@gmail.com>
2024-01-13 23:11:05 +0530

    file-provisioner configuration in terraform

---
+++ 06-provisioners/01-file-provisioners/01-provisioner.tf
@@ -0,0 +1,58 @@
+provider "aws"{
+    access_key = "give-access-key-here"
+    secret_key = "give-secret-key-here"
+    region="give-region-here"
+}
+
+resource "aws_instance" "launch-instance"{
+    ami="give the respective region os ami here"
+    instance_type="t3.micro"
+    key_name = "provis"
+    tags={
+        Name="provisioner"
+        executebyterraform=true
+    }
+
+    provisioner "file" {
+      source="D:/DevOps-Repositories/Terraform/file.txt"
+      destination="/home/ubuntu"  
+      
+    }
+
+    connection {
+      type = "ssh"
+      private_key = file("C:/Users/palla/provis.pem")
+      host = self.public_ip
+      user = "ubuntu"
+      timeout = "4m"
+
+    }
+}
+
+resource "aws_security_group" "custom-sg"{
+    description = "custom-sg-instance"
+
+    ingress {
+        description = "inbound-traffic"
+        from_port = "22"
+        to_port = "22"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    egress {
+
+        description = "outbound-traffic"
+        from_port = "22"
+        to_port = "22"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+}
+
+resource "aws_key_pair" "kpair" {
+    key_name="provis"
+    public_key = "paste the public-key here"
+
+}
